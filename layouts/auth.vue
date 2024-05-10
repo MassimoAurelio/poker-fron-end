498f1eb2f069e5b82f357545af6b2d4b16fea098
massimoaurelio <massimoaureliowork@gmail.com>
2024-05-10 16:26:24 +0300

    update

---
+++ layouts/auth.vue
@@ -0,0 +1,16 @@
+<template>
+  <section style="min-height: 100vh">
+    <div class="main-container">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.main-container {
+  padding: 5vw;
+  max-width: 1600px;
+  min-height: 1000px;
+  margin: 0 auto;
+}
+</style>
